40c92694566fbbad93e05f38ea9e2b5e68ed3d9a
Seungseok Lee <114807415+seukseok@users.noreply.github.com>
2026-03-18 22:34:17 +0900

    Title: N번 더하기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260318/N번 더하기/add-n-times.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+
+    int A, N;
+
+    cin >> A >> N;
+
+    for (int i=0; i<N; ++i){
+        A += N;
+        cout << A << "\n";
+    }
+
+    return 0;
+}
