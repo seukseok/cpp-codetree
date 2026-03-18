5d14d923ff9c1b4e88313e40c563e459570fdca8
Seungseok Lee <114807415+seukseok@users.noreply.github.com>
2026-03-18 22:08:47 +0900

    Title: 문자열 출력하기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260318/문자열 출력하기/print-string.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int N;
+
+    cin >> N;
+
+    for(int i=0; i < N;++i){
+        cout << "LeebrosCode" << "\n";
+    }
+    return 0;
+}
